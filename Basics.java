64161295e2aa3ba0f90b898e8aa59842ec5913bb
Evan Francis <enf4954@g.rit.edu>
2026-09-23 02:16:17 -0400

    more comments

--- Basics.java
+++ Basics.java
@@ -1,7 +1,14 @@
 package freshman.java;
 // The purpose of this file is for my own personal basic review of Java principles
-public class Basics{ // opening bracket of Basic class
+public class Basics{ // opening bracket of Basic class; universal convention to start class name with capital letter
     public static void main(String[] args){ // What the JVM [Java Virtual Machine] looks for to begin running code
         System.out.println("Hello World!"); // Prints "Hello World!" to the temrinal/console
+        /* println() = print line, System = built-in java class, out = member of System, short for "output" */
     }
 } // closing bracket of Basics class
+/* Syntax:
+Name of the java file must match the class name
+EX:
+    Class name: Basics
+    File name: Basics.java
+    */
