b454fd122eb74693f88aa64b6f9d378a692e8934
Evan Francis <enf4954@g.rit.edu>
2026-09-23 02:11:20 -0400

    more comments

--- Basics.java
+++ Basics.java
@@ -1,7 +1,7 @@
 package freshman.java;
 // The purpose of this file is for my own personal basic review of Java principles
-public class Basics{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+public class Basics{ // opening bracket of Basic class
+    public static void main(String[] args){ // What the JVM [Java Virtual Machine] looks for to begin running code
+        System.out.println("Hello World!"); // Prints "Hello World!" to the temrinal/console
     }
-}
+} // closing bracket of Basics class
