bee19ae5f2e787cb93a844835d4b35cfcdf23f40
Evan Francis <enf4954@g.rit.edu>
2026-09-23 02:09:57 -0400

    updated class accessibility + added comment

--- Basics.java
+++ Basics.java
@@ -1,6 +1,6 @@
 package freshman.java;
-
-class Basics{
+// The purpose of this file is for my own personal basic review of Java principles
+public class Basics{
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
